e3e1516d388db86c4399b1330235b9d2ab107194
lizhipeng <lizhipeng@p2link.cn>
2024-11-01 16:12:39 +0800

    fix bug

--- server/route.go
+++ server/route.go
@@ -31,22 +31,26 @@ func (RouteCache) Make(h any) (reflect.Type, string, error) {
 		for i := 0; i < num; i++ {
 			method := rtp.Method(i)
 			methodName := utils.ToSnakeString(method.Name)
-			if method.Type.NumIn() != 2 {
-				return nil, "", errors.New("method param num error")
-			}
 
-			if method.Type.In(0).String() != serviceName {
-				return nil, "", errors.New("method first param must type receiver")
-			}
-			if _, ok := method.Type.In(1).MethodByName("Clear"); !ok {
-				return nil, "", errors.New("method second param Request must has func Clear()")
-			}
-			rf, ok := method.Type.In(1).MethodByName("Verify")
-			if !ok {
-				return nil, "", errors.New("method second param Request must has func Verify(Context) error")
-			}
-			if rf.Type.NumOut() != 1 || !rf.Type.Out(0).Implements(tError) {
-				return nil, "", errors.New("method second param Request must has func Verify(Context) error")
+			switch method.Type.NumIn() {
+			case 2:
+				if _, ok := method.Type.In(1).MethodByName("Clear"); !ok {
+					return nil, "", errors.New("method second param Request must has func Clear()")
+				}
+				rf, ok := method.Type.In(1).MethodByName("Verify")
+				if !ok {
+					return nil, "", errors.New("method second param Request must has func Verify(Context) error")
+				}
+				if rf.Type.NumOut() != 1 || !rf.Type.Out(0).Implements(tError) {
+					return nil, "", errors.New("method second param Request must has func Verify(Context) error")
+				}
+				fallthrough
+			case 1:
+				if method.Type.In(0).String() != serviceName {
+					return nil, "", errors.New("method first param must type receiver")
+				}
+			default:
+				return nil, "", errors.New("method param num error")
 			}
 
 			numOut := method.Type.NumOut()
